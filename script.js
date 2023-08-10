08ff927f3db03b15a2f52f81d63b846214730081
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-11 05:20:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+//your code here
+// Get input from user
+let num = parseInt(prompt("Enter a number to find its factorial:"));
+
+// Initialize factorial variable
+let factorial = 1;
+
+// Find factorial using a for loop
+for (let i = 1; i <= num; i++) {
+  factorial *= i;
+}
+
+// Display result
+alert("The factorial of " + num + " is " + factorial);
